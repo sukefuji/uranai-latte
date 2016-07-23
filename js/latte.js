21031d682db21064cd42d407e42dcee7d374b1f4
takusato139git <izumi_suteado@yahoo.co.jp>
2016-07-23 14:45:14 +0900

    表示情報の成形

--- js/latte.js
+++ js/latte.js
@@ -2,7 +2,7 @@
 
 $(function () {
     $('#submit-button').click(function () {
-        paramDate = getFormattedDate(new Date());
+		paramDate = getFormattedDate(new Date());
 
         var ajax = $.ajax({
             type: 'GET',
@@ -68,7 +68,8 @@ $(function () {
     }
 
     //占い情報の表示
-	function printFortuneData(obj){	
+	function printFortuneData(obj){
+		createPrintData(obj);
 		$('#content').html(obj['content']);
 		$('#money').html(obj['money']);
 		$('#job').html(obj['job']);
@@ -79,15 +80,11 @@ $(function () {
 		$('#rank').html(obj['rank']);
 		$('#sign').html(obj['sign']);
 	}
-	function createPrintData(){
-		obj['content']
-		$('#money').html(obj['money']);
-		$('#job').html(obj['job']);
-		$('#love').html(obj['love']);
-		$('#total').html(obj['total']);
-		obj['item'] = "ラッキーアイテム :" + obj['item'];
-		obj['color'] = "ラッキーカラー :"
+	//占い情報の成形
+	function createPrintData(obj){
+		obj['item'] = "<label>ラッキーアイテム<label>" + obj['item'];
+		obj['color'] = "<label>ラッキーカラー<label>"+ obj['item'];
 		obj['rank'] = obj['rank'] + "位";
-		obj['sign'])　= "座のあなた"
+		obj['sign']　= "座のあなた";
 	}
 })
